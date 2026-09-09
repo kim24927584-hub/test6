8b70808a780998ec92d5979a196f1596ff709f4f
kim24927584-hub <kim24927584@gmail.com>
2026-09-09 18:19:04 +0900

    0909

---
+++ vscodeC/c0909/fileWrite.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+  ifstream fin("score.txt");
+  string line;
+  while(getline(fin, line)){
+    cout << line << endl;
+  }
+  fin.close();
+  return 0;
+}
---
+++ vscodeC/c0909/homework.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b);
+int main(){
+  cout << add (1,2) << endl;
+  return 0;
+}
+
+int add(int a, int b){
+  return a+ b;
+}
