039837acfce10e749791db68bc95630cd2d05565
kim24927584-hub <kim24927584@gmail.com>
2026-09-11 18:17:24 +0900

    0911

--- 0903/ediya/js/main.js
+++ 0903/ediya/js/main.js
@@ -11,9 +11,33 @@ $(function(){
     slidesToShow:1,
 
   })
+// -----------------팝업
+  
   $(".popup .btn").click(function(){
     $(".popup").css("display", "none");
   })
   $(".popup").draggable();
+
+  // ---------------햄버거
+  $(".hamburger").click(function(){
+    $(this).toggleClass("active");
+    $(".mobile_nav").toggleClass("active");
+    $(".mobile_bg").addClass("active");
+    $("body").toggleClass("hidden");
+
+  })
+  $(".mobile_bg").click(function(){
+    $(".hamburger").toggleClass("active");
+    $(".mobile_nav").toggleClass("active");
+    $(this).removeClass("active");
+    $("body").toggleClass("hidden");
+  })
+  // -------------------모바일 메뉴 항목 클릭시 아코디언
+  $(".mobile_gnb > li > a").click(function(){
+    var me =$(this).next();
+    me.slideToggle()
+    
+  })
+
 })
 
--- 0910/시험대비/slide/js/main.js
+++ 0910/시험대비/slide/js/main.js
@@ -1,6 +1,34 @@
 $(function(){
-  $(".bxslider").bxSlider({
-    auto: true,
+  
+  var slider =  $(".bxslider").bxSlider({
+    
 
+    
+    onSlideBefore:function($slideElement, oldIndex, newIndex){
+      $(".current").text(newIndex+1);
+    }
   })
+
+  $(".total").text(slider.getSlideCount())
+
+  $(".pause").on("click", function(){
+    if($(this).hasClass("on")){
+      $(this).removeClass("on");
+      slider.startAuto();
+    }else{
+      $(this).addClass("on");
+      slider.stopAuto();
+    }
+    return false;
+  })
+
+  $(".prev").on("click",function(){
+    slider.goToPrevSlide();
+    return false;
+  })
+  $(".next").on("click",function(e){
+    e.preventDefault();
+    slider.goToNextSlide();
+  })
+
 })
